4b1615fdaf7148f174d50d5e6523f3e2577970a0
Roberto Manfreda <robertomanfreda@gmail.com>
2020-02-13 01:55:33 +0100

    [ADD] - NoArgsConstructor for RLogger

--- src/main/java/com/robertomanfreda/rlogger/Main.java
+++ src/main/java/com/robertomanfreda/rlogger/Main.java
@@ -1,8 +1,10 @@
 package com.robertomanfreda.rlogger;
 
 public class Main {
-    // Uncomment, remove scopes provided from pom.xml, and run to see the power
-    /*public final static RLogger log = RLogger.builder().build();
+    /* Uncomment, remove scopes provided from pom.xml, and run to see the power
+    public final static RLogger log = RLogger.builder().build();
+    // or
+    // public final static RLogger log = new RLogger();
 
     public static void main(String[] args) {
         for (int i = 0; i < 10000; i++) {
